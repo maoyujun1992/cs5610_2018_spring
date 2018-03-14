2cb9fc7d1136d57cad24706c7ba8e0ceca00af7d
maoyujun1992 <848964604@qq.com>
2018-03-13 18:15:43 -0700

    imgae load

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
 const http = require('http');
-
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
